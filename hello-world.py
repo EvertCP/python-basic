bc051ce2a32b34d55af9359dbe5178d5bc4db80c
EvertCP <115903164+EvertCP@users.noreply.github.com>
2026-02-25 13:33:30 -0600

    Create hello-world.py to print greeting and name
    Add a script to print a greeting and full name

---
+++ hello-world.py
@@ -0,0 +1,11 @@
+#Se crean 2 variables llamadas name y lastName donde guardo mi nombre completo, despues se crea una variable llamada lastName para concatenar estos datos
+name = "Evert"
+lastName = "Cardenas"
+
+fullName = name + " " + lastName;
+
+#Para imprimir algo por consola se usa la palabra print
+print("Hola, mundo")
+
+#Puedo imprimir lo que hay dentro de una variable usando el nombre de la variable y la palabra print
+print(fullName)
